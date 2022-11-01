a31ebeedb7b8173449db2b0f296b6ee3ba1aaba1
Ng <zhen.ng@stfc.ac.uk>
2022-11-01 12:39:03 +0000

    doc strings

--- inflammation/models.py
+++ inflammation/models.py
@@ -13,22 +13,35 @@ import numpy as np
 def load_csv(filename):
     """Load a Numpy array from a CSV
 
-    :param filename: Filename of CSV to load
+    :param filename: csv Filename of CSV to load
+    :returns:text
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    :param data: integer numbers that will be used to obtain the mean
+    :returns: daily mean value
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    :param data: integer
+    :returns: max integer value
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    :param data: integer
+    :returns: min integer value
+    """
     return np.min(data, axis=0)
 
